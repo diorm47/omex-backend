1666c1c14d985363173c41b03c0656939bbd7174
diorm47 <dior47m@gmail.com>
2025-03-17 17:29:28 +0500

    server timeout

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000, 
+  })
   .then(() => {
     console.log("db ok");
     startTimerUpdate();
